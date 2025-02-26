b59187ddb6cf8f294508e4a4089b9158e06da003
anh phu <phudz25022005@gmail.com>
2025-02-26 12:16:51 +0700

    create tree

---
+++ pages/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Luxury</title>
+</head>
+<body>
+    <h1>Chào mừng đến với Luxury</h1>
+    <p>Hôm nay là: 
+        <?php echo date("d/m/Y H:i:s"); ?>
+    </p>
+</body>
+</html>
