fc11216780c21da2effdeb3c02d53893b2adb957
anh phu <phudz25022005@gmail.com>
2025-04-18 13:19:30 +0700

    hi

--- admincp/modules/category_invoice/invoice_detail.php
+++ admincp/modules/category_invoice/invoice_detail.php
@@ -1,38 +1,107 @@
 <?php
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
+// Giả định đã kết nối mysqli (biến $mysqli được thiết lập trong file include khác)
+// Kiểm tra ID đơn hàng
+if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
+    echo '<p>Lỗi: ID đơn hàng không hợp lệ.</p>';
+    exit;
+}
+$id = (int)$_GET['id'];
 
-    // Truy vấn giỏ hàng và thông tin khách hàng
-    $sql = "SELECT * FROM tbl_giohang, tbl_giohang_chitiet, tbl_dangky, tbl_vanchuyen,tbl_khuyenmai
-            WHERE tbl_giohang.id_giohang = $id
-            AND tbl_giohang.id_khachhang = tbl_dangky.id_dangky
-            AND tbl_giohang.ma_giohang = tbl_giohang_chitiet.ma_giohang
-            AND tbl_dangky.id_dangky = tbl_vanchuyen.id_dangky
-            AND tbl_giohang.id_khuyenmai = tbl_khuyenmai.id_khuyenmai
-            LIMIT 1";
+// Truy vấn thông tin giỏ hàng và khách hàng
+$sql = "SELECT 
+            g.id_giohang,
+            g.ma_giohang,
+            g.tongtien,
+            g.trangthai_giohang,
+            dk.tenkhachhang,
+            dk.email,
+            dk.dienthoai,
+            vc.ten AS ten_nguoi_nhan,
+            vc.sdt AS sdt_vanchuyen,
+            vc.diachi,
+            vc.ghi_chu,
+            km.loai_khuyenmai,
+            km.giatri
+        FROM 
+            tbl_giohang AS g
+            INNER JOIN tbl_dangky AS dk ON g.id_khachhang = dk.id_dangky
+            INNER JOIN tbl_vanchuyen AS vc ON dk.id_dangky = vc.id_dangky
+            INNER JOIN tbl_giohang_chitiet AS gct ON g.ma_giohang = gct.ma_giohang
+            LEFT JOIN tbl_khuyenmai AS km ON g.id_khuyenmai = km.id_khuyenmai
+        WHERE 
+            g.id_giohang = ?";
+$stmt = mysqli_prepare($mysqli, $sql);
+if (!$stmt) {
+    echo '<p>Lỗi truy vấn: ' . htmlspecialchars(mysqli_error($mysqli)) . '</p>';
+    exit;
+}
+mysqli_stmt_bind_param($stmt, 'i', $id);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
+$order = mysqli_fetch_assoc($result);
+mysqli_stmt_close($stmt);
 
-    $query_sql = mysqli_query($mysqli, $sql);
-    if (!$query_sql) {
-        die("Lỗi truy vấn giỏ hàng: " . mysqli_error($mysqli));
-    }
+if (!$order) {
+    echo '<p>Lỗi: Không tìm thấy đơn hàng.</p>';
+    exit;
+}
 
-    // Truy vấn danh sách sản phẩm trong giỏ hàng
-    $sql_list_sp = "SELECT tbl_sanpham.*, tbl_giohang_chitiet.soluong 
-                    FROM tbl_sanpham, tbl_giohang, tbl_giohang_chitiet
-                    WHERE tbl_giohang.id_giohang = $id
-                    AND tbl_giohang.ma_giohang = tbl_giohang_chitiet.ma_giohang
-                    AND tbl_giohang_chitiet.id_sanpham = tbl_sanpham.id_sanpham";
+// Truy vấn danh sách sản phẩm trong giỏ hàng
+$sql_products = "SELECT 
+                    sp.tensanpham,
+                    sp.giasp,
+                    sp.hinhanh,
+                    gct.soluong
+                 FROM 
+                    tbl_sanpham AS sp
+                    INNER JOIN tbl_giohang_chitiet AS gct ON sp.id_sanpham = gct.id_sanpham
+                    INNER JOIN tbl_giohang AS g ON g.ma_giohang = gct.ma_giohang
+                 WHERE 
+                    g.id_giohang = ?";
+$stmt_products = mysqli_prepare($mysqli, $sql_products);
+if (!$stmt_products) {
+    echo '<p>Lỗi truy vấn: ' . htmlspecialchars(mysqli_error($mysqli)) . '</p>';
+    exit;
+}
+mysqli_stmt_bind_param($stmt_products, 'i', $id);
+mysqli_stmt_execute($stmt_products);
+$result_products = mysqli_stmt_get_result($stmt_products);
+$products = [];
+while ($row = mysqli_fetch_assoc($result_products)) {
+    $products[] = $row;
+}
+mysqli_stmt_close($stmt_products);
 
-    $query_list_sp = mysqli_query($mysqli, $sql_list_sp);
-    if (!$query_list_sp) {
-        die("Lỗi truy vấn sản phẩm: " . mysqli_error($mysqli));
-    }
+// Cập nhật trạng thái giỏ hàng
+$sql_status = "UPDATE tbl_giohang SET trangthai_giohang = 0 WHERE id_giohang = ?";
+$stmt_status = mysqli_prepare($mysqli, $sql_status);
+if (!$stmt_status) {
+    echo '<p>Lỗi truy vấn: ' . htmlspecialchars(mysqli_error($mysqli)) . '</p>';
+    exit;
+}
+mysqli_stmt_bind_param($stmt_status, 'i', $id);
+mysqli_stmt_execute($stmt_status);
+mysqli_stmt_close($stmt_status);
 
-    // Cập nhật trạng thái giỏ hàng
-    $sql_status = "UPDATE tbl_giohang SET trangthai_giohang = 0 WHERE id_giohang = $id";
-    mysqli_query($mysqli, $sql_status);
+// Tính toán tổng tiền và thành tiền
+$total_price = 0;
+$product_count = 0;
+foreach ($products as $product) {
+    $total_price += $product['giasp'] * $product['soluong'];
+    $product_count++;
+}
+
+$final_price = $total_price;
+$discount_display = "0 VNĐ";
+if ($order['loai_khuyenmai'] == 'phantram') {
+    $final_price = $total_price * (1 - $order['giatri'] / 100);
+    $discount_display = $order['giatri'] . " %";
+} elseif ($order['loai_khuyenmai'] == 'codinh') {
+    $final_price = $total_price - $order['giatri'];
+    $discount_display = number_format($order['giatri']) . " VNĐ";
 }
 ?>
+
 <div class="container_detail">
     <div class="content_fake">
         <div class="page_detail">
@@ -48,72 +117,50 @@ if (isset($_GET['id'])) {
                         </tr>
                     </thead>
                     <tbody>
-                        <?php 
-                        $i = 0; 
-                        $tongtien = 0; 
-                        while ($product_row = mysqli_fetch_assoc($query_list_sp)) { 
-                            $i++;
-                            $tongtien += $product_row['soluong'] * $product_row['giasp']; 
-                        ?>
-                        <tr>
-                            <td id="product">
-                                <img src="modules/productMNG/image_product/<?php echo $product_row['hinhanh']; ?>" alt="anh san phẩm">
-                                <span><?php echo $product_row['tensanpham']; ?></span>
-                            </td>
-                            <td><?php echo number_format($product_row['giasp']); ?> VNĐ</td>
-                            <td><?php echo $product_row['soluong']; ?></td>
-                            <td><?php echo number_format($product_row['soluong'] * $product_row['giasp']); ?> VNĐ</td>
-                        </tr>
-                        <?php } ?>
+                        <?php if (empty($products)): ?>
+                            <tr>
+                                <td colspan="4">Không có sản phẩm trong đơn hàng.</td>
+                            </tr>
+                        <?php else: ?>
+                            <?php foreach ($products as $product): ?>
+                                <tr>
+                                    <td class="product">
+                                        <img src="modules/productMNG/image_product/<?php echo htmlspecialchars($product['hinhanh']); ?>" alt="Ảnh sản phẩm" width="50">
+                                        <span><?php echo htmlspecialchars($product['tensanpham']); ?></span>
+                                    </td>
+                                    <td><?php echo number_format($product['giasp']); ?> VNĐ</td>
+                                    <td><?php echo htmlspecialchars($product['soluong']); ?></td>
+                                    <td><?php echo number_format($product['giasp'] * $product['soluong']); ?> VNĐ</td>
+                                </tr>
+                            <?php endforeach; ?>
+                        <?php endif; ?>
                     </tbody>
                 </table>
-                <?php while ($customer_row = mysqli_fetch_assoc($query_sql)) { ?>
                 <div class="totals">
-                    <p>
-                        <p class="title">Tổng tiền:</p>
-                        <span><?php echo number_format($tongtien); ?> VNĐ</span>
-                    </p>
-                    <?php
-                    $thanhtien = $tongtien; // Initialize to avoid undefined variable
-                    if (isset($customer_row['loai_khuyenmai']) && $customer_row['loai_khuyenmai'] == 'phantram') {
-                        $thanhtien = $tongtien - $tongtien * $customer_row['giatri'] / 100;
-                    ?>
-                    <p>
-                        <p class="title">Giảm giá:</p>
-                        <span><?php echo $customer_row['giatri']; ?> %</span>
-                    </p>
-                    <?php
-                    } elseif (isset($customer_row['loai_khuyenmai']) && $customer_row['loai_khuyenmai'] == 'codinh') {
-                        $thanhtien = $tongtien - $customer_row['giatri'];
-                    ?>
-                    <p>
-                        <p class="title">Giảm giá:</p>
-                        <span><?php echo $customer_row['giatri']; ?> VNĐ</span>
-                    </p>
-                    <?php
-                    } else {
-                    ?>
-                    <p>
-                        <p class="title">Giảm giá:</p>
+                    <div class="total-item">
+                        <span class="title">Tổng tiền:</span>
+                        <span><?php echo number_format($total_price); ?> VNĐ</span>
+                    </div>
+                    <div class="total-item">
+                        <span class="title">Giảm giá:</span>
+                        <span><?php echo $discount_display; ?></span>
+                    </div>
+                    <div class="total-item">
+                        <span class="title">Thuế:</span>
                         <span>0 VNĐ</span>
-                    </p>
-                    <?php } ?>
-                    <p>
-                        <p class="title">Thuế:</p>
-                        <span>0 VNĐ</span>
-                    </p>
-                    <p>
-                        <p class="title">Tổng cộng:</p>
+                    </div>
+                    <div class="total-item">
+                        <span class="title">Tổng cộng:</span>
                         <span>
                             <?php
-                            if ($thanhtien == $customer_row['tongtien']) {
-                                echo number_format($thanhtien);
+                            if (round($final_price) == round($order['tongtien'])) {
+                                echo number_format($final_price);
                             } else {
-                                echo "Đã có lỗi xảy ra";
+                                echo "Lỗi tính toán tổng tiền";
                             }
                             ?> VNĐ
                         </span>
-                    </p>
+                    </div>
                 </div>
             </div>
             <div class="customer-details">
@@ -122,31 +169,28 @@ if (isset($_GET['id'])) {
                     <div class="customer-info">
                         <img src="../assets/images/avata.png" alt="Ảnh đại diện khách hàng" class="avatar">
                         <div class="details">
-                            <div>
-                                <p><strong><?php echo $customer_row['tenkhachhang']; ?></strong></p>
-                                <p>Mã khách hàng: #<?php echo $customer_row['id_giohang']; ?></p>
-                            </div>
+                            <p><strong><?php echo htmlspecialchars($order['tenkhachhang']); ?></strong></p>
+                            <p>Mã đơn hàng: #<?php echo htmlspecialchars($order['id_giohang']); ?></p>
                         </div>
                     </div>
                     <div class="customer-info">
-                        <img src="../assets/images/cart.png" alt="">
-                        <p><?php echo $i; ?> Sản phẩm</p>
+                        <img src="../assets/images/cart.png" alt="Giỏ hàng">
+                        <p><?php echo $product_count; ?> Sản phẩm</p>
                     </div>
                     <div class="contact">
-                        <p>Email: <a href="mailto:<?php echo $customer_row['email']; ?>"><?php echo $customer_row['email']; ?></a></p>
-                        <p>Điện thoại: <a href="tel:<?php echo $customer_row['dienthoai']; ?>"><?php echo $customer_row['dienthoai']; ?></a></p>
+                        <p>Email: <a href="mailto:<?php echo htmlspecialchars($order['email']); ?>"><?php echo htmlspecialchars($order['email']); ?></a></p>
+                        <p>Điện thoại: <a href="tel:<?php echo htmlspecialchars($order['dienthoai']); ?>"><?php echo htmlspecialchars($order['dienthoai']); ?></a></p>
                     </div>
                 </div>
                 <div class="shipping-address">
                     <h3>Thông tin vận chuyển</h3>
                     <div class="contact">
-                        <p>Người nhận: <?php echo $customer_row['ten']; ?></p>
-                        <p>Số điện thoại: <?php echo $customer_row['sdt']; ?></p>
-                        <p>Địa chỉ: <?php echo $customer_row['diachi']; ?></p>
-                        <p>Ghi chú: <?php echo $customer_row['ghi_chu']; ?></p>
+                        <p>Người nhận: <?php echo htmlspecialchars($order['ten_nguoi_nhan']); ?></p>
+                        <p>Số điện thoại: <?php echo htmlspecialchars($order['sdt_vanchuyen']); ?></p>
+                        <p>Địa chỉ: <?php echo htmlspecialchars($order['diachi']); ?></p>
+                        <p>Ghi chú: <?php echo htmlspecialchars($order['ghi_chu'] ?: 'Không có'); ?></p>
                     </div>
                 </div>
-                <?php } ?>
             </div>
         </div>
     </div>
